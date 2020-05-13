eb0597438160e55e4462185971c545d60c5ee3ef
tippitytapp <marc.tapp@gmail.com>
2020-05-13 18:58:23 -0400

    finished some of the stretch

--- queries.sql
+++ queries.sql
@@ -8,3 +8,8 @@ SELECT o.Id, s.CompanyName FROM 'Order' AS o JOIN Shipper AS s ON o.ShipVia = s.
 SELECT p.ProductName, od.Quantity, od.OrderId FROM OrderDetail AS od JOIN Product AS p ON od.ProductId = p.Id WHERE od.OrderId = 10251 ORDER BY p.ProductName;
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 SELECT o.Id AS OrderID, c.CompanyName AS CustomerCompanyName, e.LastName AS EmployeeLastName FROM [Order] AS o JOIN Customer AS c ON o.CustomerId = c.Id JOIN Employee AS e ON o.EmployeeId = e.Id;
+
+--STRETCH--
+-- Displays CategoryName and a new column called Count that shows how many products are in each category. Shows 8 records.
+SELECT c.CategoryName, COUNT('products') FROM Categories as c JOIN Products ON c.CategoryId = products.categoryId group by c.categoryname;
+-- Display OrderID and a column called ItemCount that shows the total number of products placed on the order. Shows 196 records.
